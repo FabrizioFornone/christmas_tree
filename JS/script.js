c991a284d62a95f81dd53646db137660932793c7
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-01-06 12:07:30 +0100

    christmas tree

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,30 @@
 function createBox(stampHtml) {
   let box = document.createElement("div");
-  box.className = "box-style";
+  box.classList.add("box-style");
   stampHtml.append(box);
   return box;
 }
 
 hookHtml = document.querySelector(".hook-js");
 
-let emptyArray = [];
-
-for (let i = 0; i < 8; i++) {
-  for (let i = 0; i < 11; i++) {
-    emptyArray.push(createBox(hookHtml));
+function drawTree(h) {
+  for (let i = 0; i < h; i++) {
+    let coloredSquare = "";
+    //Cambiato in coloredSquared alto poi diminuisci
+    for (let k = 1; k <= h-i; k++) {
+      //shortened to one space
+      coloredSquare = createBox(hookHtml);
+    }
+    for (let j = 0; j <= i; j++) {
+      /*Spazio aggiunto quindi c'è un numero dispari
+      di simboli e il colloredsquared sopra si adatta
+      lo spazio*/
+      coloredSquare = createBox(hookHtml);
+      coloredSquare.classList.add("colored");
+    }
+    //Moved into the loop
+    console.log(coloredSquare);
   }
 }
 
-console.log(emptyArray);
+drawTree(6);
