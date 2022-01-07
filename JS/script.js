d29b8a400881b1899b650c1df757ccaf908a6529
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-01-07 12:25:21 +0100

    Star added

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,24 @@
 // Aggancio con il DOM
 hookHtml = document.querySelector(".hook-js");
 
+let starIndex = 5;
+for (let k = 0; k < 1; k++) {
+  for (let j = 0; j < 11; j++) {
+    let box = document.createElement("div");
+    box.classList.add("box-style");
+    hookHtml.append(box);
+    if (j === starIndex) {
+      // Dichiaro boxInside, creando un elemento
+      let boxInside = document.createElement("div");
+      boxInside.classList.add("star-box");
+      // Lo stampo dentro il box principale
+      box.append(boxInside);
+    }
+  }
+}
+
 // Dichiaro un indice startIndex = 5
 let startIndex = 5;
-
 // Dichiaro un indice endIndex = 5
 let endIndex = 5;
 
@@ -21,6 +36,7 @@ for (let i = 0; i < 6; i++) {
       box.classList.add("colored");
       // Dichiaro boxInside, creando un elemento
       let boxInside = document.createElement("div");
+      boxInside.classList.add("balls-box"); 
       // Lo stampo dentro il box principale
       box.append(boxInside);
     }
@@ -30,7 +46,8 @@ for (let i = 0; i < 6; i++) {
   // Incrementa l'indice endIndex
   endIndex++;
 }
-
+// Ciclo per generare il tronco
+// Dichiaro brownIndex = 5
 let brownIndex = 5;
 for (let k = 0; k < 2; k++) {
   for (let j = 0; j < 11; j++) {
