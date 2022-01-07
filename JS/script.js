ba86dbe14678fd986379268da438621e6617dd20
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-01-07 11:48:02 +0100

    Trunk added

--- JS/script.js
+++ JS/script.js
@@ -30,3 +30,15 @@ for (let i = 0; i < 6; i++) {
   // Incrementa l'indice endIndex
   endIndex++;
 }
+
+let brownIndex = 5;
+for (let k = 0; k < 2; k++) {
+  for (let j = 0; j < 11; j++) {
+    let box = document.createElement("div");
+    box.classList.add("box-style");
+    hookHtml.append(box);
+    if (j === brownIndex) {
+      box.classList.add("trunk");
+    }
+  }
+}
